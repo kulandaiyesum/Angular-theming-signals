655d1d3b32f25cca671509da292cb3493eab34fa
kulandaiyesu <kulandaiyesu@gmail.com>
2024-06-11 15:06:37 +0530

    feat: theme change complete

--- angular-v17-theming-signal/src/app/app.component.ts
+++ angular-v17-theming-signal/src/app/app.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { HeaderComponent } from './components/header/header.component';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ThemeService } from './services/theme.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-v17-theming-signal';
+  themeService: ThemeService = inject(ThemeService);
 }
